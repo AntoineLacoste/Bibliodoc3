0d879da27b3b1d9a89948b3ece44cb2dbb25cec6
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-04-17 11:24:00 +0200

    test plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/com.msopentech.indexedDB/www/IndexedDBShim.min.js",
         "id": "com.msopentech.indexedDB.IndexedDBShim",
         "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.msopentech.indexedDB": "0.1.1"
+    "com.msopentech.indexedDB": "0.1.1",
+    "org.apache.cordova.dialogs": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
