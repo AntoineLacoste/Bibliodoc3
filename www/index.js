21a6e502fdeded9d102412ed3f23ca590bad38fd
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-04-17 12:36:32 +0200

    test lugin 13

--- www/index.js
+++ www/index.js
@@ -50,7 +50,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         alert('anduin');
-        testIDB();
+        //testIDB();
     },
 };
 
