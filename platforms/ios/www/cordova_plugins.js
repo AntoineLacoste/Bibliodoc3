68360713aa7c940f42a34b02c18c01f733635e66
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-04-17 11:11:17 +0200

    test ios

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.msopentech.indexedDB/www/IndexedDBShim.min.js",
+        "id": "com.msopentech.indexedDB.IndexedDBShim",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.msopentech.indexedDB": "0.1.1"
+}
+// BOTTOM OF METADATA
+});
