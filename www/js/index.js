1a0a3896a332c39b4a4b18444b4f85c58c640d8e
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-04-20 08:52:37 +0200

    Tst plugin 33 -bis

--- www/js/index.js
+++ www/js/index.js
@@ -45,6 +45,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        navigator.notification.alert("Coucou",null,"Coucou","Coucou");
     }
 };
 
