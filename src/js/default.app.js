2a6d0202fda84365ec3b1921fa38095600986f2f
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-05-06 15:01:28 +0200

    dr 25

--- src/js/default.app.js
+++ src/js/default.app.js
@@ -13,13 +13,13 @@
         }
 
         function initialize() {
+            catalogue.alertMssg("coucou",null,"coucou","coucou");
             var parentElement = document.getElementById("deviceready");
             var listeningElement = parentElement.querySelector('.listening');
             var receivedElement = parentElement.querySelector('.received');
 
             listeningElement.setAttribute('style', 'display:none;');
             receivedElement.setAttribute('style', 'display:block;');
-            catalogue.alertMssg("coucou",null,"coucou","coucou");
         }
     });
 })();
