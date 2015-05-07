3168001377f7c88b62cffffa8d156274374669f3
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2015-05-07 14:04:42 +0200

    module 20

--- src/js/default.app.js
+++ src/js/default.app.js
@@ -15,7 +15,7 @@
         function initialize() {
             //catalogue.alertMssg("oucou",null);
 
-                console.log("toto");
+                console.log("toto" + test1());
                 //console.log("" + test());
 
             var parentElement = document.getElementById("deviceready");
